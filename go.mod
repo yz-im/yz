b2982e1b53520b2897be540575b52ce6aa42da66
terry <terrychaoc@gmail.com>
2023-02-25 10:45:12 +0800

    feat: init demo

--- go.mod
+++ go.mod
@@ -1,3 +1,18 @@
 module github.com/yz-im/yz
 
 go 1.20
+
+require (
+	github.com/gobwas/ws v1.1.0
+	github.com/klintcheng/chatdemo v0.0.0-20210809014858-f1bc9333b64b
+	github.com/sirupsen/logrus v1.9.0
+	github.com/spf13/cobra v1.6.1
+)
+
+require (
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"flag"
+
+	"github.com/klintcheng/chatdemo/client"
+	"github.com/klintcheng/chatdemo/serv"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+const version = "v1"
 
 func main() {
-	fmt.Println("hello yz")
+	flag.Parse()
+
+	root := &cobra.Command{
+		Use:     "chat",
+		Version: version,
+		Short:   "chat demo",
+	}
+	ctx := context.Background()
+
+	root.AddCommand(serv.NewServerStartCmd(ctx, version))
+	root.AddCommand(client.NewCmd(ctx))
+
+	if err := root.Execute(); err != nil {
+		logrus.WithError(err).Fatal("Could not run command")
+	}
 }
